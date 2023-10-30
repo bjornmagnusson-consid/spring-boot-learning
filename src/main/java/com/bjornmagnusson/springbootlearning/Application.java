5b90d70778f97c5b9fae39222050caf85336da03
Björn Magnusson <bjorn.magnusson@consid.se>
2023-10-30 17:59:20 +0100

    Added cors headers for api from localhost origins

--- src/main/java/com/bjornmagnusson/springbootlearning/Application.java
+++ src/main/java/com/bjornmagnusson/springbootlearning/Application.java
@@ -1,12 +1,29 @@
 package com.bjornmagnusson.springbootlearning;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		LOG.info("Adding cors");
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/*").allowedOriginPatterns("*localhost*");
+			}
+		};
 	}
 }
